fc55631bd1cd22dc93120762dd3e8d0d72c471b7
Josh Mervine <jmervine@yp.com>
2013-03-12 14:19:17 -0700

    updating default source url

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 default['subway']['user']        = "root"
-default['subway']['src_url']     = "https://github.com/jmervine/subway.git"
+default['subway']['src_url']     = "git://github.com/thedjpetersen/subway.git"
 default['subway']['git_branch']  = "master"
 
 # this will not be used unless user == 'root'
