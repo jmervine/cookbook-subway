82184f6cbc22d54e5157a57aea0ae91afe3cdd47
Josh Mervine <jmervine@yp.com>
2013-03-12 13:49:34 -0700

    updating version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "joshua@mervine.net"
 license          "MIT"
 description      "Installs/Configures subway"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 depends          "git"
 depends          "nginx"
 depends          "nodejs"
